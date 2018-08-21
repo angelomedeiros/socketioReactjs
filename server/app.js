fcc1521fb2c04158b5cc59ff7eaf62b9410ce250
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-21 20:29:50 -0300

    Iniciando configuracao do socketio

--- server/app.js
+++ server/app.js
@@ -8,6 +8,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var io  = require('socket.io')()
+
+app.io = io
+
+io.on('connection', function(socket) {
+  console.log('Conexão estabelecida!')
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
